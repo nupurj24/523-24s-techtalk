1b1f89a763cc2a008725c496ecd7c4a319398460
Nupur Joshi <nupurj24@email.unc.edu>
2024-03-31 21:17:07 -0400

    Completed what my part of the demo will end up like.

--- DemoApp/App.js
+++ DemoApp/App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, TextInput, FlatList, SectionList } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View accessible={true}>
       <StatusBar style="auto" />
+      <Text>Some text That's not ideal</Text>  
+      <View>
+        <Text>Some new text that starts where we want </Text>
+        <Text> React Native Core Components </Text>
+        <Text> View, StatusBar, Text, Image, Button, ScrollView, TextInput </Text>
+        <Image
+          source={require ('./assets/uterus.png')
+          }
+          style={{width: 350, height: 200, }}
+        />
+      </View>
+      <TextInput
+        
+        defaultValue="You can type in me"
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
