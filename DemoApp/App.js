0f0b4e795dfcfcd964a11e76f08f518496965304
Amanda Xiao <amanda64@email.unc.edu>
2024-03-31 22:22:08 -0400

    Create Button and Style

--- DemoApp/App.js
+++ DemoApp/App.js
@@ -1,20 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, TextInput, FlatList, SectionList, Button, Pressable } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ScrollView accessible={true}>
       <StatusBar style="auto" />
-    </View>
+      <Text>Some text That's not ideal</Text>  
+      <View>
+        <Text>Some new text that starts where we want </Text>
+        <Text> React Native Core Components </Text>
+        <Text> View, StatusBar, Text, Image, Button, ScrollView, TextInput </Text>
+        <Image
+          source={require ('./assets/uterus.png')
+          }
+          style={{width: 350, height: 200, }}
+        />
+      </View>
+      <TextInput
+        
+        defaultValue="You can type in me"
+      />
+      <Pressable style={styles.button}>
+      {/* style={({pressed}) => [
+        {backgroundColor: pressed ? 'rgb(210, 230, 255)' : 'pink',}]}> */}
+        <Text style={styles.text}>Submit</Text>
+      </Pressable>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  button: {
     alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 4,
+    backgroundColor: 'pink',
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
